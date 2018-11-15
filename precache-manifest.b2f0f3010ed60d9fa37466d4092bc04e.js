97d75137aa1c110721f46255545a5d8d42129304
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-15 08:52:20 +0000

    Deploy chialin/ghpages_demo to github.com/chialin/ghpages_demo.git:gh-pages

---
+++ precache-manifest.b2f0f3010ed60d9fa37466d4092bc04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ddf81b153264710f749",
+    "url": "/ghpages_demo/static/css/main.e1f1e51a.chunk.css"
+  },
+  {
+    "revision": "5ddf81b153264710f749",
+    "url": "/ghpages_demo/static/js/main.5ddf81b1.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/ghpages_demo/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "99dd61efca9389961130",
+    "url": "/ghpages_demo/static/js/runtime~main.99dd61ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages_demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec631612b457fab5c349b01611591698",
+    "url": "/ghpages_demo/index.html"
+  }
+];
